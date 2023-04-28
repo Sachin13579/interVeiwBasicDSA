5e325aee18182ce02aac0e1145fba9c0c72d2599
Sachin13579 <singh.sachin@resourcifi.com>
2023-04-29 02:34:58 +0530

    few more pattrn question added

--- patternPrinting.js/pattern.js
+++ patternPrinting.js/pattern.js
@@ -14,14 +14,105 @@
 // squarePattern(5)
 
 
-function rightAngleTriangle(n){
-    let str=""
-    for (let i = 0; i < n; i++) {
-        // const element = array[i];
 
-        str+="*"
-        console.log(str)
-    }
+// printe pattern like----------------------------------------------------------------------
+// 1,2,3,4
+// 1,2,3,4
+// 1,2,3,4
+// 1,2,3,4
+
+// function squareWithNuber(n){
+
+//     let i=1
+//     let str=""
+//     while (i<=n) {
+//         let j=1
+//         while (j<=n) {
+//             str+=j+" "
+//             j++
+//         }
+//         str+="\n"
+// i++
+//     }
+//     console.log(str);
+
+// }
+// squareWithNuber(4)
+
+// lets print incremeting Count pattern----------------------------------------------------------
+// function squareWithNuber(n){
+
+//     let i=1
+
+//     let str=""
+//     let count=1
+//     while (i<=n) {
+    //         let j=1
+    //         while (j<=n) {
+//             str+=count+" "
+//             count++
+//             j++
+//         }
+//         str+="\n"
+// i++
+//     }
+//     console.log(str);
+
+// }
+// squareWithNuber(4)
+
+
+// ----/-----------------------------------------/---TRIANGLES------/------------------------------------/--
+
+    // function rightAngleTriangle(n){
+    //     let str=""
+    //     for (let i = 0; i < n; i++) {
+    //         // const element = array[i];
+    //         for (let j=0;j<=i;j++){
+                
+    //             str+="*"
+    //         }
+    //         str+="\n"
     
+    //     }
+    //     console.log(str)
+        
+    // }
+    // rightAngleTriangle(4)
+
+// triangle with Numbers
+// 1
+// 12
+// 123
+
+const numberTriangle=(n)=>{
+        let str=""
+    for(let i=1;i<=n;i++){
+        
+        for(let j=1;j<=i;j++){
+                str+=i-j+1
+                
+
+        }
+        str+="\n"
+    }
+    console.log(str)
+
 }
-rightAngleTriangle(4)
+numberTriangle(4)
+// const numberTriangle=(n)=>{
+//             let str=""
+//         for(let i=1;i<=n;i++){
+//             // let val=i
+//             for(let j=1;j<=i;j++){
+//                     str+=i+j-1
+//                     // val++
+    
+//             }
+//             str+="\n"
+//         }
+//         console.log(str)
+    
+//     }
+//     numberTriangle(4)
+
